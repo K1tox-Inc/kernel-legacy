5d1124d29c6de7bb2a0ebde5953c222a6d6eb699
jbettini <ashhxtm@gmail.com>
2025-12-21 21:06:49 +0100

    dev(proc): create lock header

--- src/proc/lock.c
+++ src/proc/lock.c
@@ -1,22 +1,6 @@
-#include <types.h>
+#include <proc/lock.h>
 
-typedef volatile uint8_t spin_lock_t;
-
-static volatile uint32_t lock_counter = 0;
-
-static inline void lock_irq(void)
-{
-	__asm__ volatile("cli");
-	lock_counter++;
-};
-
-static inline void unlock_irq(void)
-{
-	if (lock_counter)
-		lock_counter--;
-	if (!lock_counter)
-		__asm__ volatile("sti");
-};
+volatile uint32_t lock_counter = 0;
 
 void spin_lock(spin_lock_t *lock)
 {
