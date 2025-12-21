d5939235181b7536359cc0ba4336fa2553575e0c
jbettini <ashhxtm@gmail.com>
2025-12-21 20:59:53 +0100

    dev(proc): add lock for futur preemption

--- src/proc/lock.c
+++ src/proc/lock.c
@@ -4,7 +4,7 @@ volatile uint32_t lock_counter = 0;
 
 void spin_lock(spin_lock_t *lock)
 {
-	lock_irq(); // Contains the memory barrier via CLI clobber
+	lock_irq();
 	*lock = 1;
 	/* On Single-Core, disabling IRQs ensures mutual exclusion
 	 * For Multi-Core, need an atomic Test-And-Set loop here :
@@ -15,11 +15,8 @@ void spin_lock(spin_lock_t *lock)
 	 */
 }
 
-// Also the memory barrier currently inside lock_irq and unlock_irq
-// they will be move inside the functions spins if multi core is implemented
-
 void spin_unlock(spin_lock_t *lock)
 {
 	*lock = 0;
-	unlock_irq(); // Contains the memory barrier via STI clobber
+	unlock_irq();
 }
