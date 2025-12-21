99cbd136d09071ea33ce6adb77d2dab236ff7baa
jbettini <ashhxtm@gmail.com>
2025-12-21 21:29:59 +0100

    fix(proc): add memory clobber

--- src/proc/lock.c
+++ src/proc/lock.c
@@ -4,18 +4,23 @@ volatile uint32_t lock_counter = 0;
 
 void spin_lock(spin_lock_t *lock)
 {
-	lock_irq();
+	lock_irq(); // Contains the memory barrier via CLI clobber
 	*lock = 1;
 	/* On Single-Core, disabling IRQs ensures mutual exclusion
 	 * For Multi-Core, need an atomic Test-And-Set loop here :
 	 *
 	 * while(*lock)
 	 *  Attomic ops;
+	 *
+	 *
 	 */
 }
+
+// Also the memory barrier currently inside lock_irq and unlock_irq
+// they will be move inside the functions spins if multi core is implemented
 
 void spin_unlock(spin_lock_t *lock)
 {
 	*lock = 0;
-	unlock_irq();
+	unlock_irq(); // Contains the memory barrier via STI clobber
 }
