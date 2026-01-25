084cde56a5b0b1bd93989889ca253196953c61c5
jbettini <ashhxtm@gmail.com>
2026-01-26 00:59:17 +0100

    dev(irq): fix asm to use trap frame instead of reg

--- src/arch/x86/irq.s
+++ src/arch/x86/irq.s
@@ -8,27 +8,39 @@
 	.extern interrupt_handlers
 
 syscall_dispatcher:
-	mov eax, [esp+36]
+	# Pass pointer to trap_frame as argument
+	lea eax, [esp]
+	push eax
+	
+	# Get int_no from trap_frame (offset: 4 segs + 8 GPRs = 48 bytes)
+	mov eax, [esp+52]  # +4 because we pushed the pointer
 	shl eax, 2
 	mov eax, [syscall_handlers+eax]
 
 	test eax, eax
 	jz .no_sys_handler
 
-	jmp eax
+	call eax
+	add esp, 4  # Clean up pushed pointer
 
 .no_sys_handler:
 	ret
 
 interrupt_dispatcher:
-	mov eax, [esp+40]
+	# Pass pointer to trap_frame as argument
+	lea eax, [esp]
+	push eax
+	
+	# Get int_no from trap_frame (offset: 4 segs + 8 GPRs = 48 bytes)
+	mov eax, [esp+52]  # +4 because we pushed the pointer
 	shl eax, 2
 	mov eax, [interrupt_handlers+eax]
 
 	test eax, eax
 	jz .no_int_handler
 
-	jmp eax
+	call eax
+	add esp, 4  # Clean up pushed pointer
 
 .no_int_handler:
 	ret
@@ -36,16 +48,26 @@ interrupt_dispatcher:
 interrupt_routine:
 	pusha
 
+	# Push all segment registers (order: ds, es, fs, gs)
 	mov ax, ds
 	push eax
+	mov ax, es
+	push eax
+	mov ax, fs
+	push eax
+	mov ax, gs
+	push eax
 
+	# Load kernel data segment
 	mov ax, 0x10
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
 
-	mov eax, [esp+36]
+	# Send EOI to PIC
+	# Adjust offset: 4 segments * 4 bytes + 8 GPRs * 4 bytes = 48 bytes
+	mov eax, [esp+48]
 	cmp eax, 40
 	jl .skip_pic2
 
@@ -56,13 +78,23 @@ interrupt_routine:
 	mov al, 0x20
 	out 0x20, al
 
+	# Pass pointer to trap_frame as argument
+	lea eax, [esp]
+	push eax
+	
 	call interrupt_dispatcher
+	
+	add esp, 4  # Clean up pushed pointer
 
+	# Restore segment registers (reverse order: gs, fs, es, ds)
+	pop eax
+	mov gs, ax
+	pop eax
+	mov fs, ax
+	pop eax
+	mov es, ax
 	pop eax
 	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
 
 	popa
 	add esp, 4
