97159bd32b10cd47458b280b1b99aa219b177579
Noah Alexandre <noah.alxdr+github@gmail.com>
2026-02-12 08:44:59 +0100

    feat(context_switch): `push`/`pop` "Callee-Saved" registers

--- src/proc/context.s
+++ src/proc/context.s
@@ -4,10 +4,24 @@
 .extern g_tss
 
 # tss + 4 => esp0
+# task + 36 => esp
 # task + 40 => cr3
 # task + 44 => kernel_stack_ptr
 
-# void switch_to(struct task_struct *prev, struct task_struct *next);
+# void switch_to(struct task *current, struct task *next);
 switch_to:
+  push ebp
+  mov ebp, esp
+
+  push ebx
+  push esi
+  push edi
+
+  pop edi
+  pop esi
+  pop ebx
+
+  mov esp, ebp
+  pop ebp
 
   ret
