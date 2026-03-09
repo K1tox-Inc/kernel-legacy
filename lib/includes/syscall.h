68b8b33380dcde958a8ab9aa80280a8549d61869
jbettini <ashhxtm@gmail.com>
2026-03-09 05:13:29 +0100

    fix(syscall): bad path and indentation

---
+++ lib/includes/syscall.h
@@ -0,0 +1,4 @@
+/* auto-generated FILE - do not edit */
+
+#define sys_write 0
+#define sys_fork 1
--- scripts/generate_table.py
+++ scripts/generate_table.py
@@ -1,10 +1,18 @@
-syscall_dir = "../src/syscall"
+import os
+
+script_dir   = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(script_dir)
+
+syscall_dir  = os.path.join(project_root, "src", "syscall")
+user_headers = os.path.join(project_root, "lib", "includes")
 
 def main():
     try:
-        with open(f"{syscall_dir}/syscall_table.c", "w") as sys_header:
-            sys_header.write("/* auto-generated FILE - do not edit */\n\n")
-            sys_header.write("void *syscall_table[] = {\n")
+        with open(f"{syscall_dir}/syscall_table.c", "w") as sys_table, open(f"{user_headers}/syscall.h", "w") as sys_user_header:
+            sys_table.write("/* auto-generated FILE - do not edit */\n\n")
+            sys_user_header.write("/* auto-generated FILE - do not edit */\n\n")
+
+            sys_table.write("void *syscall_table[] = {\n")
             file = open(f'{syscall_dir}/syscall.tbl')
             content = file.readlines()
             for line in content:
@@ -24,8 +32,10 @@ def main():
                     if not sys_entry.isidentifier():
                         raise ValueError("Bad value: syscall.tbl: sys_entry id must be a valid identifier in C")
 
-                    sys_header.write(f"    [{syscall_id}] = {sys_entry},\n")
-            sys_header.write("};\n")
+                    sys_table.write(f"    [{syscall_id}] = {sys_entry},\n")
+                    sys_user_header.write(f"#define {sys_entry} {syscall_id}\n")
+
+            sys_table.write("};\n")
             
     except FileNotFoundError as e:
         print(f"FileNotFoundError: {str(e)}")
