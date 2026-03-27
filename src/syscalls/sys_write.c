13a9eba3726f708fb1ead2efadfc1366eb0ca5b9
jbettini <ashhxtm@gmail.com>
2026-03-27 02:40:20 +0100

    dev(syscalls): fix sys_write

--- src/syscalls/sys_write.c
+++ src/syscalls/sys_write.c
@@ -1,19 +1,29 @@
 #include <drivers/vga.h>
 #include <libk.h>
 #include <memory/kmalloc.h>
+#include <memory/usercopy.h>
 #include <syscalls/syscalls.h>
+#include <utils/error.h>
 
 SYSCALL_DEFINE3(write, int, fd, const char *, str, size_t, size)
 {
 	(void)fd;
 
+	if (size > MAX_KMALLOC_SIZE)
+		return -EINVAL;
 	char *dup = kmalloc(size + 1, __GFP_KERNEL);
-	ft_memcpy(dup, str, size);
+	if (!dup)
+		return -ENOMEM;
+	int ret = copy_from_user(dup, str, size);
+	if (ret) {
+		kfree(dup);
+		return -EFAULT;
+	}
 	dup[size] = 0;
 
 	vga_printf("%s", dup);
 
 	kfree(dup);
 
-	return 0;
+	return size;
 }
