1cc6438769589d8cc0f3a9aeba4398e69b135989
jbettini <ashhxtm@gmail.com>
2026-04-05 03:29:39 +0200

    dev(waitqueue): follow intgate_32 design

--- include/proc/waitqueue.h
+++ include/proc/waitqueue.h
@@ -7,13 +7,18 @@
 // STRUCTS & MACROS
 // ============================================================================
 struct task;
+struct wq_entry;
 
 typedef void (*finish_handler)(struct wq_entry *entry);
 
+enum wq_state { TASK_INTERRUPTIBLE, TASK_UNINTERRUPTIBLE };
+
 struct wq_entry {
 	struct task     *task;
-	finish_handler   handler;
+	struct wq_head  *head;
 	struct list_head node;
+	enum wq_state    state;
+	finish_handler   handler;
 };
 
 struct wq_head {
@@ -25,14 +30,17 @@ struct wq_head {
 // ============================================================================
 
 static inline void wq_init(struct wq_head *wq) { INIT_SENTINEL(&wq->head); }
-static inline void wq_entry_init(struct wq_entry *entry, struct task *task)
+static inline void wq_entry_init(struct wq_entry *entry, struct task *task, enum wq_state state)
 {
 	entry->task    = task;
+	entry->head    = NULL;
 	entry->handler = NULL;
+	entry->state   = state;
 	INIT_SENTINEL(&entry->node);
 }
 
 void wq_prepare(struct wq_head *wq, struct task *task);
 void wq_finish(struct wq_entry *entry);
+void wq_wake_specifiq(struct task *task);
 void wq_wake_one(struct wq_head *wq);
 void wq_wake_all(struct wq_head *wq);
