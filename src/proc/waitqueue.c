143f668bb292cb38a42cb9104ccf2146c3b76bc3
jbettini <ashhxtm@gmail.com>
2026-04-05 03:29:39 +0200

    dev(waitqueue): follow intgate_32 design

--- src/proc/waitqueue.c
+++ src/proc/waitqueue.c
@@ -1,26 +1,33 @@
-#include <proc/lock.h>
 #include <proc/task.h>
 #include <proc/waitqueue.h>
 
 void wq_prepare(struct wq_head *wq, struct task *task)
 {
-	lock_preempt(&task->lock);
+	task->wq_data.head = wq;
 	task->state        = TASK_WAITING;
 	task->need_resched = true;
 	list_add_head(&task->wq_data.node, &wq->head);
-	// this call rescedule the process
-	unlock_preempt(&task->lock);
 }
 
 void wq_finish(struct wq_entry *entry)
 {
-	lock_preempt(&entry->task->lock);
-	pop_node(&entry->node);
-	if (entry->handler)
-		entry->handler(entry);
-	entry->task->state = TASK_RUNNING;
-	// sched_enqueue(entry->task);
-	unlock_preempt(&entry->task->lock);
+    if (!list_node_is_linked(&entry->node))
+        return;
+    pop_node(&entry->node);
+    INIT_SENTINEL(&entry->node); 
+    if (entry->handler)
+        entry->handler(entry);
+    entry->task->state = TASK_RUNNING;
+    entry->head        = NULL;
+    
+    // sched_enqueue(entry->task);
+}
+
+void wq_signal_wake_up(struct task *task)
+{
+	if (task->wq_data.state == TASK_UNINTERRUPTIBLE)
+		return;
+	wq_wake_specifiq(task);
 }
 
 void wq_wake_specifiq(struct task *task) { wq_finish(&task->wq_data); }
