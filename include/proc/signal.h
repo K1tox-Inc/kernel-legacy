a75ea27b04b1a6a0009adc0fdce5f65587938c52
jbettini <ashhxtm@gmail.com>
2026-04-17 01:07:56 +0200

    dev(signals): add some primitive

--- include/proc/signal.h
+++ include/proc/signal.h
@@ -5,41 +5,41 @@
 struct task;
 
 enum signals {
-	SIGHUP    = 1,  /* Hangup */
-	SIGINT    = 2,  /* Interrupt (Ctrl+C) */
-	SIGQUIT   = 3,  /* Quit */
-	SIGILL    = 4,  /* Illegal instruction */
-	SIGTRAP   = 5,  /* Trace/breakpoint trap */
-	SIGABRT   = 6,  /* Aborted */
-	SIGIOT    = 6,  /* Alias pour SIGABRT */
-	SIGBUS    = 7,  /* Bus error */
-	SIGFPE    = 8,  /* Floating point exception */
-	SIGKILL   = 9,  /* Killed (force quit) */
-	SIGUSR1   = 10, /* User defined signal 1 */
-	SIGSEGV   = 11, /* Segmentation fault */
-	SIGUSR2   = 12, /* User defined signal 2 */
-	SIGPIPE   = 13, /* Broken pipe */
-	SIGALRM   = 14, /* Alarm clock */
-	SIGTERM   = 15, /* Terminated */
-	SIGSTKFLT = 16, /* Stack fault */
-	SIGCHLD   = 17, /* Child exited */
-	SIGCLD    = 17, /* Alias pour SIGCHLD */
-	SIGCONT   = 18, /* Continued */
-	SIGSTOP   = 19, /* Stopped (signal) */
-	SIGTSTP   = 20, /* Stopped (user) */
-	SIGTTIN   = 21, /* Stopped (tty input) */
-	SIGTTOU   = 22, /* Stopped (tty output) */
-	SIGURG    = 23, /* Urgent I/O condition */
-	SIGXCPU   = 24, /* CPU time limit exceeded */
-	SIGXFSZ   = 25, /* File size limit exceeded */
-	SIGVTALRM = 26, /* Virtual timer expired */
-	SIGPROF   = 27, /* Profiling timer expired */
-	SIGWINCH  = 28, /* Window changed */
-	SIGPOLL   = 29, /* I/O possible */
-	SIGIO     = 29, /* Alias pour SIGPOLL */
-	SIGPWR    = 30, /* Power failure */
-	SIGSYS    = 31, /* Bad system call */
-	Sentinel  = 32  /* Sentinel */
+	SIGHUP       = 1,  /* Hangup */
+	SIGINT       = 2,  /* Interrupt (Ctrl+C) */
+	SIGQUIT      = 3,  /* Quit */
+	SIGILL       = 4,  /* Illegal instruction */
+	SIGTRAP      = 5,  /* Trace/breakpoint trap */
+	SIGABRT      = 6,  /* Aborted */
+	SIGIOT       = 6,  /* Alias pour SIGABRT */
+	SIGBUS       = 7,  /* Bus error */
+	SIGFPE       = 8,  /* Floating point exception */
+	SIGKILL      = 9,  /* Killed (force quit) */
+	SIGUSR1      = 10, /* User defined signal 1 */
+	SIGSEGV      = 11, /* Segmentation fault */
+	SIGUSR2      = 12, /* User defined signal 2 */
+	SIGPIPE      = 13, /* Broken pipe */
+	SIGALRM      = 14, /* Alarm clock */
+	SIGTERM      = 15, /* Terminated */
+	SIGSTKFLT    = 16, /* Stack fault */
+	SIGCHLD      = 17, /* Child exited */
+	SIGCLD       = 17, /* Alias pour SIGCHLD */
+	SIGCONT      = 18, /* Continued */
+	SIGSTOP      = 19, /* Stopped (signal) */
+	SIGTSTP      = 20, /* Stopped (user) */
+	SIGTTIN      = 21, /* Stopped (tty input) */
+	SIGTTOU      = 22, /* Stopped (tty output) */
+	SIGURG       = 23, /* Urgent I/O condition */
+	SIGXCPU      = 24, /* CPU time limit exceeded */
+	SIGXFSZ      = 25, /* File size limit exceeded */
+	SIGVTALRM    = 26, /* Virtual timer expired */
+	SIGPROF      = 27, /* Profiling timer expired */
+	SIGWINCH     = 28, /* Window changed */
+	SIGPOLL      = 29, /* I/O possible */
+	SIGIO        = 29, /* Alias pour SIGPOLL */
+	SIGPWR       = 30, /* Power failure */
+	SIGSYS       = 31, /* Bad system call */
+	SIG_Sentinel = 32  /* SIG_Sentinel */
 };
 
 static inline const char *signal_to_string(enum signals sig)
@@ -57,7 +57,7 @@ static inline const char *signal_to_string(enum signals sig)
 	    [SIGWINCH] = "SIGWINCH",   [SIGPOLL] = "SIGPOLL",     [SIGPWR] = "SIGPWR",
 	    [SIGSYS] = "SIGSYS",
 	};
-	if (sig < 1 || sig >= Sentinel)
+	if (sig < 1 || sig >= SIG_Sentinel)
 		return "UNKNOWN";
 	return names[sig] ? names[sig] : "UNKNOWN";
 }
@@ -67,3 +67,5 @@ bool signal_check_perm(struct task *target);
 void signal_send(enum signals sig, struct task *dst);
 bool signal_is_set(enum signals sig, struct task *dst);
 void signal_reset(struct task *dst);
+void signal_dequeue(enum signals sig, struct task *dst);
+int  signal_dequeue_yield(struct task *task);
--- src/proc/signal.c
+++ src/proc/signal.c
@@ -1,7 +1,8 @@
 #include <proc/signal.h>
 #include <proc/task.h>
+#include <utils/error.h>
 
-bool signal_is_valid(enum signals sig) { return (sig >= 0 && sig < Sentinel); }
+bool signal_is_valid(enum signals sig) { return (sig >= 0 && sig < SIG_Sentinel); }
 
 bool signal_check_perm(struct task *target)
 {
@@ -20,13 +21,13 @@ bool signal_check_perm(struct task *target)
 void signal_send(enum signals sig, struct task *dst)
 {
 	// Here we need to handle task state
-	if (dst && sig > 0 && sig < Sentinel)
-		dst->signals_map |= (1 << sig);
+	if (dst && sig > 0 && sig < SIG_Sentinel)
+		dst->signals_map |= (1U << sig);
 }
 
 bool signal_is_set(enum signals sig, struct task *dst)
 {
-	if (dst && sig < Sentinel) {
+	if (dst && sig < SIG_Sentinel) {
 		return (dst->signals_map >> sig) & 1;
 	}
 	return false;
@@ -36,4 +37,23 @@ void signal_reset(struct task *dst)
 {
 	if (dst)
 		dst->signals_map = 0;
+}
+
+void signal_dequeue(enum signals sig, struct task *dst)
+{
+	if (dst && sig > 0 && sig < SIG_Sentinel)
+		dst->signals_map &= ~(1U << sig);
+}
+
+int signal_dequeue_yield(struct task *task)
+{
+	if (!task)
+		return -ESRCH;
+	for (int i = 1; i < SIG_Sentinel; i++) {
+		if (!((task->signals_map >> i) & 1U))
+			continue;
+		signal_dequeue(i, task);
+		return i;
+	}
+	return 0;
 }
