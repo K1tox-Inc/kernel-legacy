0351341896f218df0835ec2845132f4d9014b058
Noah Alexandre <noah.alxdr+github@gmail.com>
2026-07-03 01:43:21 +0200

    dev(io.h): Add `outl` `inl` and `inw`

--- include/arch/io.h
+++ include/arch/io.h
@@ -2,20 +2,41 @@
 
 #include <types.h>
 
+static __always_inline void outl(uint16_t port, uint32_t value)
+{
+	__asm__ volatile("outl %0, %1" : : "a"(value), "Nd"(port) : "memory");
+}
+
 static __always_inline void outw(uint16_t port, uint16_t value)
 {
-	__asm__ volatile("outw %w0, %w1" : : "a"(value), "Nd"(port) : "memory");
+	__asm__ volatile("outw %0, %1" : : "a"(value), "Nd"(port) : "memory");
 }
 
 static __always_inline void outb(uint16_t port, uint8_t value)
 {
-	__asm__ volatile("outb %b0, %w1" : : "a"(value), "Nd"(port) : "memory");
+	__asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port) : "memory");
+}
+
+static __always_inline uint32_t inl(uint16_t port)
+{
+	uint32_t value;
+
+	__asm__ volatile("inl %1, %0" : "=a"(value) : "Nd"(port) : "memory");
+	return value;
+}
+
+static __always_inline uint16_t inw(uint16_t port)
+{
+	uint16_t value;
+
+	__asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port) : "memory");
+	return value;
 }
 
 static __always_inline uint8_t inb(uint16_t port)
 {
 	uint8_t value;
 
-	__asm__ volatile("inb %w1, %b0" : "=a"(value) : "Nd"(port) : "memory");
+	__asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port) : "memory");
 	return value;
 }
