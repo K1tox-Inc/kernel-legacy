f4050f2b8c43a2889788f1d82fecea644a8824a4
Noah Alexandre <noah.alxdr+github@gmail.com>
2026-09-21 13:39:50 +0200

    fix(io.h): Replace asm `inb` by asm `inw` in `inw` function

--- include/arch/io.h
+++ include/arch/io.h
@@ -29,7 +29,7 @@ static __always_inline uint16_t inw(uint16_t port)
 {
 	uint16_t value;
 
-	__asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port) : "memory");
+	__asm__ volatile("inw %1, %0" : "=a"(value) : "Nd"(port) : "memory");
 	return value;
 }
 
